3de7374da807d74f3c47b0a261a5fa65eb6e53d2
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-04-01 23:37:29 +0900

    ADD chapter1

--- vue-todo-app/components/TodoCreator.vue
+++ vue-todo-app/components/TodoCreator.vue
@@ -1,3 +1,27 @@
 <template>
-    <h2>Todo creator</h2>
+    <div>
+        <button @click="createTodo">추가</button>
+        <input     
+        :value="title"
+        :placeholder="placeholder"
+        type="text"
+        @input="title = $event.target.value"
+        @keypress.enter="createTodo">
+    </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            title:'',
+            placeholder:'할일을 추가해주세요',
+        }
+    },
+    methods:{
+        createTodo(){
+            console.log(this.title)
+        }
+    }
+}
+</script>
